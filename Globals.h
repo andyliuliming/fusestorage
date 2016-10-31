4d9ecfee63c6843cd7c388d833c5b28213364e5f
andyliuliming <andyliuliming@outlook.com>
2016-10-31 05:06:30 -0400

    remove dead code

--- Globals.h
+++
@@ -1,9 +0,0 @@
-#ifndef globals_hh
-#define globals_hh
-#include "AzureStorageConfig.h"
-#include "AzureStorageFSEnv.h"
-
-AzureStorageConfig *azureStorageConfig;
-AzureStorageFSEnv *azureStorageFSEnv;
-
-#endif //globals_hh
